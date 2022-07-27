1f4d80b33533f3af75fa0c9f37da27d2ab39af4a
Kant <98311953+Kladex@users.noreply.github.com>
2022-07-27 13:03:13 +0700

    Update ex-1.js

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,12 @@
+// Exercise #1: Restaurant Listing
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 
-// Start coding here
+// Start coding For Loop here
+
+
+
+// Start coding While Loop here
+
+
+
+// Start coding For of here
